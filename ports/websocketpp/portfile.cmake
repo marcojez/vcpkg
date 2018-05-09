5f9085ea4c603ac33cac7676b8363bae446c3b7b
Marco Jez <marco.jez@gmail.com>
2018-05-09 09:32:18 +0200

    Updated websocketpp to latest develop because of compatibility problems with C++17

--- ports/websocketpp/portfile.cmake
+++ ports/websocketpp/portfile.cmake
@@ -4,16 +4,16 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO zaphoyd/websocketpp
-    REF 0.7.0
-    SHA512 91a86d4f5120db3f474169bb146f865f82167b1e9eedabec8793b31005e4ce3d22083283bc1b9f9e37fa0da835addcb2b68260a27c753852c06b3b1bb2f3c12e
-    HEAD_REF master
+    REF 19cad9925f83d15d7487c16f0491f4741ec9f674
+    SHA512 7027975c434c38e182130da7185d3dc2e7777bf69ebfc8e4df5d9d8c66157afed1ff59217d23d1b39a05794d2692f97acb147be3592cdc92a6f322458ae42f4e
+    HEAD_REF develop
 )
 
-vcpkg_apply_patches(
-    SOURCE_PATH ${SOURCE_PATH}
-    PATCHES
-        ${CMAKE_CURRENT_LIST_DIR}/openssl_110.patch
-)
+#vcpkg_apply_patches(
+#    SOURCE_PATH ${SOURCE_PATH}
+#    PATCHES
+#        ${CMAKE_CURRENT_LIST_DIR}/openssl_110.patch
+#)
 
 file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/share/websocketpp)
 
@@ -24,7 +24,7 @@ file(RENAME ${CURRENT_PACKAGES_DIR}/share/websocketpp/COPYING ${CURRENT_PACKAGES
 # Copy the header files
 file(COPY "${SOURCE_PATH}/websocketpp" DESTINATION "${CURRENT_PACKAGES_DIR}/include" FILES_MATCHING PATTERN "*.hpp")
 
-set(INSTALL_INCLUDE_DIR "\${CMAKE_CURRENT_LIST_DIR}/../../include")
+set(PACKAGE_INSTALL_INCLUDE_DIR "\${CMAKE_CURRENT_LIST_DIR}/../../include")
 set(WEBSOCKETPP_VERSION 0.7.0)
 configure_file(${SOURCE_PATH}/websocketpp-config.cmake.in "${CURRENT_PACKAGES_DIR}/share/websocketpp/websocketpp-config.cmake" @ONLY)
-configure_file(${SOURCE_PATH}/websocketpp-configVersion.cmake.in "${CURRENT_PACKAGES_DIR}/share/websocketpp/websocketpp-configVersion.cmake" @ONLY)
+#configure_file(${SOURCE_PATH}/websocketpp-configVersion.cmake.in "${CURRENT_PACKAGES_DIR}/share/websocketpp/websocketpp-configVersion.cmake" @ONLY)
